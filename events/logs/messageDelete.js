bed4d8a5d0ce05afc74b077100a1569ff4a2c293
Djibril <142848494+serial-checker@users.noreply.github.com>
2024-11-10 05:59:09 +0100

    Create messageDelete.js

---
+++ events/logs/messageDelete.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const db = require("quick.db")
+const Discord = require("discord.js");
+const ms = require("ms")
+
+module.exports = (client, message) => {
+    
+	let guild = message.guild
+	const color = db.get(`color_${guild.id}`) === null ? client.config.color : db.get(`color_${guild.id}`)
+	let wass = db.get(`msglog_${message.guild.id}`);
+	const logschannel = message.guild.channels.cache.get(wass)
+
+	if (logschannel) logschannel.send(new Discord.MessageEmbed()
+        .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({dynamic : true }))
+        .setColor(color)
+		.setDescription(`**Message supprimé** dans <#${message.channel.id}> par ${message.author}\n ${message.content}`)
+		//.addField(`${message.content}`, `ㅤㅤㅤㅤㅤㅤㅤ`)
+		//.setFooter(`${client.config.name}`)
+		.setTimestamp())
+}
