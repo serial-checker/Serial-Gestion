50c0471f70011d7140d44570bf81040307acf92d
Djibril <142848494+serial-checker@users.noreply.github.com>
2024-11-13 12:07:30 +0100

    more posibilities and fix bug

--- events/logs/voiceStreamingStart.js
+++ events/logs/voiceStreamingStart.js
@@ -1,19 +1,27 @@
 const axios = require('axios');
-const db = require("quick.db")
+const db = require("quick.db");
 const Discord = require("discord.js");
-const ms = require("ms")
+const ms = require("ms");
 
-module.exports = (client, member, voiceChannel, message) => {
-	
-	const color = db.get(`color_${member.guild.id}`) === null ? client.config.color : db.get(`color_${member.guild.id}`)
-	let wass = db.get(`logvc_${voiceChannel.guild.id}`);
-	const logschannel = voiceChannel.guild.channels.cache.get(wass)
-    
-	if (logschannel) logschannel.send(new Discord.MessageEmbed()
-		.setColor(color)
-        .setAuthor(`${member.user.username}`, member.user.displayAvatarURL({dynamic : true }))
-		.setDescription(`${member} **partage son écran** dans <#${voiceChannel.id}>`)
-		//.setFooter(`${client.config.name}`)
-		.setTimestamp()
-	)
-}
+module.exports = (client, member, voiceChannel) => {
+
+    const color = db.get(`color_${member.guild.id}`) === null ? client.config.color : db.get(`color_${member.guild.id}`);
+    let wass = db.get(`logvc_${voiceChannel.guild.id}`);
+    const logschannel = voiceChannel.guild.channels.cache.get(wass);
+
+    // Si un canal de log est défini
+    if (logschannel) {
+        const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setAuthor(`${member.user.username}`, member.user.displayAvatarURL({ dynamic: true }))
+            .setDescription(`${member} **partage son écran** dans <#${voiceChannel.id}>`)
+            .addField('ID du salon', voiceChannel.id, true)
+            .addField('Nom du salon', voiceChannel.name, true)
+            .addField('Statut', `${member.presence ? member.presence.status : 'Non défini'}`, true)
+            .addField('Heure du partage', new Date().toLocaleString(), true)
+            .setTimestamp();
+
+        // Envoi de l'embed dans le canal de logs
+        logschannel.send(embed);
+    }
+};
