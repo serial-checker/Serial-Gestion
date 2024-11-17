cd3a588664296ea6056c3e4b0d4a418f3a50415a
Djibril <142848494+serial-checker@users.noreply.github.com>
2024-11-17 12:36:48 +0100

    add footer

--- events/logs/voiceChannelLeave.js
+++ events/logs/voiceChannelLeave.js
@@ -21,6 +21,7 @@ module.exports = async (client, member, channel) => {
             .addField('Statut', `${member.presence ? member.presence.status : 'Non défini'}`, true)
             .addField('Ancien salon', member.voice.channel ? `#${member.voice.channel.name} (ID: ${member.voice.channel.id})` : 'Aucun', true)
             .addField('Heure de déconnexion', new Date().toLocaleString(), true)
+            .setFooter(`${client.config.name ? client.config.name : 'Bot'} | Log de salon vocal`)
             .setTimestamp();
 
         // Envoi de l'embed dans le canal de logs
