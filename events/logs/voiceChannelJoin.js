03320590de55a34aea807cb6f6fb6642859ee721
Djibril <142848494+serial-checker@users.noreply.github.com>
2024-12-31 01:50:52 +0100

    reduce embed detaill

--- events/logs/voiceChannelJoin.js
+++ events/logs/voiceChannelJoin.js
@@ -14,18 +14,7 @@ module.exports = async (client, member, channel, message) => {
         const embed = new Discord.MessageEmbed()
             .setColor(color)
             .setAuthor(`${member.user.username}`, member.user.displayAvatarURL({ dynamic: true }))
-            .setDescription(`${member} se **connecte au salon** <#${channel.id}>`)
-            .addField('Nom du salon vocal', channel.name, true)
-            .addField('ID du salon vocal', channel.id, true)
-            .addField('Nombre d\'utilisateurs dans le salon', channel.members.size, true)
-            .addField('Salon privé', channel.type === 'voice' && channel.permissionsFor(member.guild.roles.everyone).has('VIEW_CHANNEL') ? 'Public' : 'Privé', true)
-            .addField('Statut du membre', `${member.presence ? member.presence.status : 'Non défini'}`, true)
-            .addField('Membre ID', member.id, true)
-            .addField('Roles du membre', member.roles.cache.map(role => role.name).join(', ') || 'Aucun', true)
-            .addField('Heure de connexion', new Date().toLocaleString(), true)
-            .setThumbnail(member.user.displayAvatarURL({ dynamic: true })) // Ajoute l'avatar du membre en miniature
-            .setFooter(`${client.config.name ? client.config.name : 'Bot'} | Log de salon vocal`)
-            .setTimestamp();
+            .setDescription(`${member} se connecte au salon ${channel.name}`)
 
         // Envoi de l'embed dans le canal de logs
         logschannel.send(embed);
