8dc2955854ed2ed4fc4af9f56212c62b665f12b3
Djibril <142848494+serial-checker@users.noreply.github.com>
2024-12-31 01:51:12 +0100

    reduce detaill embed

--- events/logs/voiceChannelLeave.js
+++ events/logs/voiceChannelLeave.js
@@ -15,14 +15,7 @@ module.exports = async (client, member, channel) => {
         const embed = new Discord.MessageEmbed()
             .setColor(color)
             .setAuthor(`${member.user.username}`, member.user.displayAvatarURL({ dynamic: true }))
-            .setDescription(`${member} quitte **le salon** <#${channel.id}>`)
-            .addField('ID du salon', channel.id, true)
-            .addField('Nom du salon', channel.name, true)
-            .addField('Statut', `${member.presence ? member.presence.status : 'Non défini'}`, true)
-            .addField('Ancien salon', member.voice.channel ? `#${member.voice.channel.name} (ID: ${member.voice.channel.id})` : 'Aucun', true)
-            .addField('Heure de déconnexion', new Date().toLocaleString(), true)
-            .setFooter(`${client.config.name ? client.config.name : 'Bot'} | Log de salon vocal`)
-            .setTimestamp();
+            .setDescription(`${member} quitte le salon ${channel.name}`)
 
         // Envoi de l'embed dans le canal de logs
         logschannel.send(embed);
