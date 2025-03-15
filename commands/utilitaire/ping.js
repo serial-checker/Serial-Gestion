6dcfc252b34a8dd8a76e051326efbb3f4b9aba20
Djibril <142848494+serial-checker@users.noreply.github.com>
2025-03-16 00:47:06 +0100

    add fields ... better version

--- commands/utilitaire/ping.js
+++ commands/utilitaire/ping.js
@@ -1,52 +1,44 @@
-const Discord = require('discord.js')
-const {
-	MessageEmbed
-} = require('discord.js')
-const db = require('quick.db')
-const {
-	MessageActionRow,
-	MessageButton,
-	MessageMenuOption,
-	MessageMenu
-} = require('discord-buttons');
+const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const db = require('quick.db');
 
-function sleep(ms) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, ms)
-	})
-}
 module.exports = {
-	name: 'ping',
-	aliases: ["speed", "api"],
+    name: 'ping',
+    aliases: ["speed", "api"],
 
-	run: async (client, message, args, prefix, color) => {
+    run: async (client, message, args, prefix, color) => {
 
-		let perm = ""
-		message.member.roles.cache.forEach(role => {
-			if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true
-			if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true
-			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true
-		})
-		if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm || db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true) {
-			
-			let embeed = new Discord.MessageEmbed()
-			//embeed.setTitle("")
-			embeed.addField("Ping", `Calcul en cours...`, true)
-			embeed.addField("Latence", `${client.ws.ping}ms`, true)
-			embeed.setColor(color)
-			embeed.setTimestamp()
-			embeed.setFooter(`${client.config.name}`)
+        let perm = "";
+        message.member.roles.cache.forEach(role => {
+            if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true;
+            if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true;
+            if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true;
+        });
 
-			let msg = await message.channel.send(embeed)
-			let embed = new Discord.MessageEmbed()
-			//embed.setTitle("")
-			embed.addField("Discord Api :", `${msg.createdAt - message.createdAt + "ms"}`, true)
-			embed.addField(`Latence : ${client.user.username}`, `${client.ws.ping}ms`, true)
-			embed.setColor(color)
-			embed.setTimestamp()
-			embed.setFooter(`${client.config.name}`)
+        if (client.config.owner.includes(message.author.id) || 
+            db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || 
+            perm || 
+            db.get(`channelpublic_${message.guild.id}_${message.channel.id}`) === true) {
+            
+            let embedLoading = new MessageEmbed()
+                .addField("Ping", `Calcul en cours...`, true)
+                .addField("Latence", `${client.ws.ping}ms`, true)
+                .setColor(color)
+                .setTimestamp()
+                .setFooter(client.config.name);
 
-			return msg.edit("", embed)
-		}
-	}
-}
+            let msg = await message.channel.send(embedLoading);
+
+            const ping = Date.now() - message.createdTimestamp;
+
+            let embedResult = new MessageEmbed()
+                .addField("Discord API :", `${ping}ms`, true)
+                .addField(`Latence : ${client.user.username}`, `${client.ws.ping}ms`, true)
+                .setColor(color)
+                .setTimestamp()
+                .setFooter(client.config.name);
+
+            return msg.edit(embedResult);
+        }
+    }
+};
