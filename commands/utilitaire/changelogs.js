7a87528476a169ea612fbe538aff0762eb1740ea
Djibril <142848494+serial-checker@users.noreply.github.com>
2025-03-23 15:14:50 +0100

    fix bugs

--- commands/utilitaire/changelogs.js
+++ commands/utilitaire/changelogs.js
@@ -19,34 +19,28 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
             .setColor(color)
-            .setTitle("📢 Changements récents")
+            .setTitle("Changements récents")
             .setDescription("[Voir le projet sur GitHub](https://github.com/serial-checker/Serial-Gestion)\n\n✨ **Dernières améliorations et ajouts sur le bot :**")
             .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
             .addField("🆕 **Ajouts récents :**", 
                 "• ✨ **`prevname`** : *Affiche l'historique des pseudos d'un utilisateur.*\n" +
-                "• 🎭 **`rolemembers`** : *Liste les membres d'un rôle mentionné.*\n" +
-                "• 🚀 **`boosters`** : *Montre les membres ayant boosté le serveur.*\n" +
-                "• 🧮 **`calc`** : *Permet d'effectuer un calcul mathématique.*\n" +
-                "• 📊 **`rank`** : *Affiche le rang d'un membre.*\n" +
                 "• 🐌 **`cfx`** : *Permet de voir l'état des serveurs CFX.re*\n" +
-                "• 🎧 **`bringall`** : *Déplace tous les membres vers un salon vocal précis.*\n" +
-                "• 🎧 **`bring`** : *Déplace un membres vers un salon vocal précis.*\n" +
+                "• 🎧 **`bring [all]`** : *Déplace un ou tout les membres vers un salon vocal précis.*\n" +
                 "• 🔧 **`delrole`** : *Retire un rôle à un membre.*\n" +
                 "• 👄 **`fakesay`** : *La commande envoie un message en se faisant passer pour un membre*\n" +
-                "• 🕰️ **`slowmode`** : *Active le mode lent dans un salon (max 6h).*\n" +
                 "• 🚀 **`dmall`** : *Permet d'envoyer un message en fonction du rôle choisis à tout les membres le possédant*\n" +
-                "• 🎁 **`nitro`** : *Permet d'envoyer un code nitro*\n" +                  
+                "• 🎁 **`nitro`** : *Permet d'envoyer un code nitro*\n" +
+                "• 📍 **`checkreact [ping]`** : *Permet de voir les réaction d'un rôle bien précis sur un message bien précis et aussi de ping*\n" +           
                 "• 🤖 **`mybot`** : *Affiche tous les bots perso que vous possedez*\n\u200B")
             .addField("🔧 **Rework :**", 
-                "• 🎯 **`top rank`** & **`top invite`** : *Amélioration des classements.*\n" +
-                "• 🔞 **`porngif`** : *Ajout de catégorie*\n" +
-                "• 🎨 **`help`** & **`help all`** : *Rework esthétique.*\n" +
-                "• 🔑 **Permissions :** *Amélioration de la gestion des accès sur toutes les commandes.*\n\u200B")
+                "• 🔞 **`porngif`** : *Ajout de catégorie pornographique*\n" +
+                "• 🎨 **`help`** & **`help all`** : *Modification de certaine perm pour certaine commande*\n" +
+                "• 🔑 **Permissions :** *Fix de toute les permissions sur toutes les commandes*\n\u200B")
             .addField("📊 **Logs ajoutées :**", 
-                "• 🛡️ **`roleUpdate`** : *Suivi des modifications de rôles.*\n" +
-                "• 🔄 **`VoiceChannelSwitch`** : *Détection des changements de salon vocal.*\n" +
-                "• 🎙️ **`voiceMuteLogs`** : *Logs des actions de mute/démute.*\n" +
-                "• 🎧 **`voiceStateUpdate`** : *Suivi des changements d'état vocal.*")
+                "• 🛡️ **`voiceChannelJoin`** : *La logs s'affiche enfin.*\n" +
+                "• 🛡️ **`voiceChannelLeave`** : *La logs s'affiche enfin*\n" +
+                "• 🛡️ **`voiceStreamingStrart`** : *La logs s'affiche enfin*\n" +
+                "• 🎧 **`voiceStreamingStop`** : *La logs s'affiche enfin*")
             .setFooter("🕰️ Dernière mise à jour :")
             .setTimestamp();
 
